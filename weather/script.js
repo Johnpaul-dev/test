0e126241b849217173460384ddb5b1e688adbda6
Johnpaul-dev <nexusiwill@outlook.com>
2022-09-13 15:54:39 +0100

    first

---
+++ weather/script.js
@@ -0,0 +1,32 @@
+const state = document.getElementById("header");
+let latitude = "";
+let longitude = "";
+
+//http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API key} f24baf01aff33594366c6279cff93512
+
+const weather = () => {
+    fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=5&appid=f24baf01aff33594366c6279cff93512`)
+    .then(res => res.json())
+    .then(data => 
+        state.textContent = data[0].state
+        )
+} 
+
+const getLocation = () => {
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+
+const onSuccess = (position) => {
+    // console.log(position.coords);
+    latitude = position.coords.latitude;
+    longitude = position.coords.latitude;
+
+    weather();
+}
+
+const onError = () => {
+    console.log("error");
+}
+
+getLocation();
+
