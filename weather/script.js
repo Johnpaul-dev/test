4d9496027da88108766b9af674737a40e2a8098e
Johnpaul <nexusiwill@outlook.com>
2022-09-14 10:30:17 +0100

    Update script.js

--- weather/script.js
+++ weather/script.js
@@ -5,7 +5,7 @@ let longitude = "";
 //http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API key} f24baf01aff33594366c6279cff93512
 
 const weather = () => {
-   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f24baf01aff33594366c6279cff93512`)
+   fetch(`https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&appid=f24baf01aff33594366c6279cff93512`)
     .then(res => res.json())
     .then(data => 
           console.log(data.sys.country)
